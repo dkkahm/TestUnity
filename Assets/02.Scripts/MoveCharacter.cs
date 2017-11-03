fb403f5bcedfc984d29ccf4867b9288435a771df
Kam Dongki <dkkahm@gmail.com>
2017-11-03 10:13:31 +0900

    MoveCharacter #1

---
+++ Assets/02.Scripts/MoveCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour {
+	Transform m_character;
+
+	public void SetCharacter(Transform character) {
+		m_character = character;
+	}
+
+	public void MoveForward(float distance = 1.0f) {
+		m_character.Translate(Vector3.forward * distance);
+	}
+
+	public void MoveBackward(float distance = 1.0f) {
+		m_character.Translate(Vector3.back * distance);
+	}
+
+	public void MoveLeft(float distance = 1.0f) {
+		m_character.Translate(Vector3.left * distance);
+	}
+
+	public void MoveRight(float distance = 1.0f) {
+		m_character.Translate(Vector3.right * distance);
+	}
+
+	public void TurnLeft(float rad = 0.1f) {
+		m_character.Rotate(-1f * Vector3.up * rad * Mathf.Rad2Deg);
+	}
+
+	public void TurnRight(float rad = 0.1f) {
+		m_character.Rotate(Vector3.up * rad * Mathf.Rad2Deg);
+	}
+
+	public void Roll(float rad = 0.1f) {
+		m_character.Rotate(Vector3.forward * rad * Mathf.Rad2Deg);
+	}
+}
---
+++ Assets/02.Scripts/Tank.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+	MoveCharacter m_move_character;
+
+	// Use this for initialization
+	void Start () {
+		m_move_character = GetComponent<MoveCharacter>();
+		m_move_character.SetCharacter(this.transform);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hor = Input.GetAxis("Horizontal");
+		float ver = Input.GetAxis("Vertical");
+
+		m_move_character.MoveForward(ver);
+		m_move_character.TurnRight(hor * Mathf.Deg2Rad * 2f);
+
+		float roll = Input.GetButton("Jump") ? 1.0f : 0.0f;
+		Debug.Log(roll);
+		m_move_character.Roll(roll * Mathf.Deg2Rad);
+	}
+}
