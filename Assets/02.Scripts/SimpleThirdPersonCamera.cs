6c1d0ea3e53a6f6a074e41b7388d825d166c9c9d
Kam Dongki <dkkahm@gmail.com>
2017-11-03 14:10:49 +0900

    Update SimpleThirdPersonCamera

---
+++ Assets/02.Scripts/SimpleThirdPersonCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleThirdPersonCamera : MonoBehaviour {
+	Transform m_camera = null;
+
+	Transform m_player = null;
+	Vector3 m_player_pivot = new Vector3();
+	Vector3 m_camera_pos_from_pivot = new Vector3(0f, 1.0f, -1.0f);
+
+	float m_speed_camera_move = 0.3f;
+	float m_speed_camera_rotation = 0.3f;
+
+	public void SetCamera(Transform camera) {
+		m_camera = camera;
+	}
+
+	public void SetPlayer(Transform player, Vector3 pivot, Vector3 camera_pos_from_pivot) {
+		m_player = player;
+		m_player_pivot = pivot;
+		m_camera_pos_from_pivot = camera_pos_from_pivot;
+	}
+
+	public void SetSpeed(float speed_move, float speed_rotation) {
+		m_speed_camera_move = speed_move;
+		m_speed_camera_rotation = speed_rotation;
+	}
+
+	public void UpdateCamera(float delta_time) {
+		if(m_camera != null && m_player != null) {
+			Vector3 pivot_pos = m_player.TransformPoint(m_player_pivot);
+			// Debug.Log("povot:" + pivot_pos);
+			Vector3 camera_pos_target = m_player.TransformPoint(m_player_pivot + new Vector3(0f, 0f, m_camera_pos_from_pivot.z)) + new Vector3(0f, m_camera_pos_from_pivot.y, 0f);
+			// Debug.Log("camera_pos_target:" + camera_pos_target);
+			Quaternion camera_lookat_target = Quaternion.LookRotation(pivot_pos - camera_pos_target);
+
+			Vector3 camera_pos = Vector3.Lerp(m_camera.transform.position, camera_pos_target, m_speed_camera_move);
+			Quaternion camera_lookat = Quaternion.Slerp(m_camera.transform.rotation, camera_lookat_target, m_speed_camera_rotation);
+
+			m_camera.transform.position = camera_pos;
+			m_camera.transform.rotation = camera_lookat;
+		}
+	}
+}
--- Assets/02.Scripts/Tank.cs
+++ Assets/02.Scripts/Tank.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Tank : MonoBehaviour {
 	MoveCharacter m_move_character;
 
+	SimpleThirdPersonCamera m_camera;
+
 	// Use this for initialization
 	void Start () {
 		m_move_character = GetComponent<MoveCharacter>();
 		m_move_character.SetCharacter(this.transform);	
+
+		m_camera = GetComponent<SimpleThirdPersonCamera>();
+		m_camera.SetPlayer(this.transform, new Vector3(), new Vector3(0f, 2f, -4f));
+		m_camera.SetCamera(Camera.main.transform);
 	}
 	
 	// Update is called once per frame
@@ -20,7 +26,10 @@ public class Tank : MonoBehaviour {
 		m_move_character.TurnRight(hor * Mathf.Deg2Rad * 2f);
 
 		float roll = Input.GetButton("Jump") ? 1.0f : 0.0f;
-		Debug.Log(roll);
 		m_move_character.Roll(roll * Mathf.Deg2Rad);
+
+		m_camera.UpdateCamera(Time.deltaTime);
+
+		Debug.Log(Camera.main.transform.position);
 	}
 }
